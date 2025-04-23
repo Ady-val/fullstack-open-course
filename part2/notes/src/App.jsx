d544b46fe4d461a70b0e12f01dd02280f74ed8cc
Adal Valles <adal.vallesb@gmail.com>
2025-04-23 13:43:29 -0600

    part2.11

--- part2/notes/src/App.jsx
+++ part2/notes/src/App.jsx
@@ -1,9 +1,10 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 import Note from './components/Note'
 
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+const App = () => {
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState(
     'a new note...'
   )
@@ -11,6 +12,17 @@ const App = (props) => {
   const notesToShow = showAll
   ? notes
   : notes.filter(note => note.important === true)
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }, [])
+  console.log('render', notes.length, 'notes')
 
   const addNote = (event) => {
     event.preventDefault()
--- part2/phone-book/src/App.jsx
+++ part2/phone-book/src/App.jsx
@@ -1,14 +1,21 @@
 import { useState } from 'react'
+import axios from 'axios'
 import PhoneBook from './components/PhoneBook'
-import Input from './components/Input'
 import Filter from './components/Filter'
 import PhonebookForm from './components/PhonebookForm'
+import { useEffect } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '555-555-5555' }
-  ])
+  const [persons, setPersons] = useState([])
   const [nameFilter, setNameFilter] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   const filteredPersons = data => {
     if (!nameFilter) return data;
